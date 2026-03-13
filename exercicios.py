536b813abe827537cec742b718d3bcdb38e07088
VictorManzini <vikmanzini@icloud.com>
2026-03-13 14:19:13 -0300

    exercicios da aula 1

---
+++ exercicios.py
@@ -0,0 +1,9 @@
+x = 5
+y = 10
+z = 5 / y 
+x = x + 1
+y = z 
+z = y + x
+print(x)
+print(y)
+print(z)
---
+++ exercicios2.py
@@ -0,0 +1,9 @@
+x = 0
+y = 0
+z = 0
+Resultado = x + y + z
+x = 10 
+y = 25
+z = 30
+reultado = x + y + z
+print(Resultado)
